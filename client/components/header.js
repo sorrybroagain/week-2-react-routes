07203f6f3b76a4f897d68f6437bd917b6d6d5b07
esli <test@Esli-Mac-2.local>
2020-10-14 23:09:58 +0300

    task 2 is done

--- client/components/header.js
+++ client/components/header.js
@@ -26,9 +26,10 @@ const Header = () => {
       </div>
       {toggled && (
         <div id="menu" className="block">
-          <button
+          <a
             type="button"
             className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+            href="https://www.google.com/"
           >
             <svg
               className="fill-current h-3 w-3"
@@ -38,7 +39,7 @@ const Header = () => {
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
-          </button>
+          </a>
         </div>
       )}
     </nav>
